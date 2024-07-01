8d9587f2c6bd90de8bb996a8861e4d68c076f7af
Rohan Talagadadeevi <59935451+rohant20@users.noreply.github.com>
2024-07-01 13:35:47 -0400

    Delete dayBy directory

--- dayBy/README.md
+++
@@ -1,7 +0,0 @@
-# DayBy Planner App by Rohan Talagadadeevi
-
-A planner app that takes care of the planning and lets you live life day By day
-
-***
-More Information Soon
-***
--- dayBy/src/ListNameInput/PrioBTN/PrioBTN.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./PrioBTN.module.css";
-
-export default function PrioBTN(props) {
-    const btnClassName = "btnScaleAsc" + props.btnLabel;
-    return (
-        <button onClick={props.clickFunc} type="button" className={`btn ${styles.btnScale} ${styles[btnClassName]} `}>{props.btnLabel}</button>
-    );
-}
